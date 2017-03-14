196ace8f0bdf8b9e8f8dcd8f557a5108199bdbbe
enali <enalix@163.com>
2017-03-14 09:46:36 +0800

    修改了run.sh, 使用了函数

--- run.sh
+++ run.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
-ln -s bd_env $HOME/.bd_env
-ln -s bd_aliases $HOME/.bd_aliases
+
+## create symlink
+sym_link() {
+  fn = $HOME/.$1
+  if [ -f $fn ]; then
+    rm $fn
+  fi
+  ln -s $1 $fn
+}
+
+file_list="bd_env bd_aliases"
+for f in file_list; do
+  sym_link($f)
+done
